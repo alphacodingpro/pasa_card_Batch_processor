3f1cb8ce263c8e54dd6131acb73e8944b9391a45
muhammadubaidyousuf <ubaidahmedmeo@gmail.com>
2026-04-04 09:29:53 +0500

    fix region mismatch and postgres sqlalchemy diallect

--- backend/database.py
+++ backend/database.py
@@ -9,6 +9,8 @@ load_dotenv()
 # Get the PostgreSQL connection URL from the environment variables
 # Format expected: postgresql://username:password@localhost/dbname
 SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_URL", "postgresql://mac@localhost/postgres")
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
